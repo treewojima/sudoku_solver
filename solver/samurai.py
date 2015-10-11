63ec10fb61363035f8b64fdbddb057dfb446a60c
Scott Bishop <treewojima@gmail.com>
2015-10-11 19:17:06 -0400

    started work on samurai logic

--- solver/samurai.py
+++ solver/samurai.py
@@ -6,5 +6,22 @@ class Samurai(object):
 
         box1 = sudokus[0].get_box(2, 2)
         box2 = sudokus[4].get_box(0, 0)
-        for i in range(0, 9):
-            pass
+        merge_boxes(box1, box2)
+
+        box1 = sudokus[1].get_box(0, 2)
+        box2 = sudokus[4].get_box(2, 0)
+        merge_boxes(box1, box2)
+
+        box1 = sudokus[2].get_box(2, 0)
+        box2 = sudokus[4].get_box(0, 2)
+        merge_boxes(box1, box2)
+
+        box1 = sudokus[3].get_box(2, 2)
+        box2 = sudokus[5].get_box(0, 0)
+        merge_boxes(box1, box2)
+
+def merge_boxes(box1, box2):
+    for i in range(0, 9):
+        box2[i].candidates = box1[i].candidates
+
+samurai = Samurai()
